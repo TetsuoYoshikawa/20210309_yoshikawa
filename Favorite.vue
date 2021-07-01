6954df10b531120e145351513790e692f840196b
TetsuoYoshikawa <77278965+TetsuoYoshikawa@users.noreply.github.com>
2021-07-01 17:18:19 +0900

    Add files via upload

---
+++ Favorite.vue
@@ -0,0 +1,21 @@
+<template>
+  <vue-ster animate="animated rubberBand" color="#F05654">
+    <a slot="icon" class="fa fa-heart" @click="handleClick"></a>
+  </vue-ster>
+</template>
+
+<script>
+export default{
+  mounted:{
+    handleClick(){
+      this.$refs.ThumsUp.$data.active = true;
+      console.log(this.$refs.ThumsUp.$data);
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.ThumbsUp.$data);
+    }
+  }
+}
+</script>
